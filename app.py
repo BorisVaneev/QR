1cdb64252c697e98e100f2cac46be95432878aae
BorisVaneev <47080432+BorisVaneev@users.noreply.github.com>
2025-04-11 16:21:19 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
 import qrcode
+import os  # Добавляем эту строку
 
 def generate_qrcode(text):
-    qr=qrcode.QRCode(
-        version =1,
+    qr = qrcode.QRCode(
+        version=1,
         error_correction=qrcode.constants.ERROR_CORRECT_L,
         box_size=10,
         border=4,
     )
     qr.add_data(text)
     qr.make(fit=True)
-    img=qr.make_image(fill_color="black",back_color="white")
+    img = qr.make_image(fill_color="black", back_color="white")
     img.save("qrimg.png")
 
-url=qrcode.getenv("NAME", "default_user") 
+# Исправленная строка:
+url = os.getenv("NAME", "default_user")  # Теперь правильно!
 generate_qrcode(url)
